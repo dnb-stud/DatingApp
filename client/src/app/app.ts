6093fb7b87b6c845a215aba0714eaeef62a35ffd
dnb-stud <basstorniloo@rambler.ru>
2026-07-28 23:42:30 +0300

    End of section 6

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,32 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from '../features/home/home';
 import { IUser } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-  private accountService = inject(AccountService);
   private readonly http = inject(HttpClient);
+  protected router = inject(Router);
   protected readonly title = signal('Date app');
   protected readonly members = signal<IUser[]>([]);
+  public get isItHomePath() { return this.router.url === '/'; };
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
   }
 
   private async getMembers() {
@@ -36,6 +28,6 @@ export class App implements OnInit {
     } catch (error) {
       console.log(error);
       throw error;
-    } 
+    }
   }
 }
---
+++ client/src/core/services/toast-service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  private containerId = 'toast-container';
+
+  constructor() {
+    this.createToastContainer();
+  }
+  
+  private createToastContainer() {
+    if (!document.getElementById(this.containerId)) {
+      const container = document.createElement('div');
+      container.id = this.containerId;
+      container.className = 'toast toast-botton toast-end';
+      document.body.appendChild(container);
+    }
+  }
+
+  private createToastElement(message: string, alertClass: string, duration = 5000) {
+    const toastContainer = document.getElementById(this.containerId);
+    if (!toastContainer) return;
+
+    const toast = document.createElement('div');
+    toast.classList.add('alert', alertClass, 'shadow-lg');
+    toast.innerHTML = `
+      <span>${message}</span>
+      <button class="ml-4 btn btn-sm bnt-ghost">x</button>
+    `;
+
+    toast.querySelector('button')?.addEventListener('click', () => {
+      toastContainer.removeChild(toast);
+    });
+
+    toastContainer.append(toast);
+
+    setTimeout(() => {
+      if (toastContainer.contains(toast)) {
+        toastContainer.removeChild(toast);
+      }
+    }, duration);
+  }
+
+  success(message: string, duration?: number) {
+    this.createToastElement(message, 'alert-success', duration);
+  }
+
+  error(message: string, duration?: number) {
+    this.createToastElement(message, 'alert-error', duration);
+  }
+
+  warning(message: string, duration?: number) {
+    this.createToastElement(message, 'alert-warning', duration);
+  }
+
+  info(message: string, duration?: number) {
+    this.createToastElement(message, 'alert-info', duration);
+  }
+}
